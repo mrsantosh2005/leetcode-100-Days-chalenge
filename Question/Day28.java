24868da8c870aea3a6bc6f90fcf9b7137bfa50f4
mrsantosh2005 <schintawar80@gmail.com>
2025-12-18 21:39:07 +0530

    Day28

---
+++ Question/Day28.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Day28 {
+
+    public static void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+
+        sortColors(nums);
+
+        System.out.println("Sorted Colors: " + Arrays.toString(nums));
+    }
+}
