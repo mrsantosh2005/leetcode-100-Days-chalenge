b25b07a2fe21dd5b7b128639edf162856a4564bc
mrsantosh2005 <schintawar80@gmail.com>
2025-12-19 17:13:25 +0530

    Day30

---
+++ Question/Day30.java
@@ -0,0 +1,28 @@
+public class Day30 {
+
+    // LeetCode-compatible method
+    public static int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+
+    // For VS Code execution
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(height)); // Output: 6
+    }
+}
